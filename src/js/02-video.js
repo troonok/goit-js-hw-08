b22ea7514a3a2cd3c5015009e77f13f32b511f16
Anastasiia Kandalintceva <valeryanaromanova@gmail.com>
2023-01-16 01:39:25 +0200

    task2 vimeo+lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
+ const iframe = document.querySelector('iframe');
+ const player = new Player(iframe);
+
+ player.on('play', function() {
+     console.log('played the video!');
+   
+ });
+
+ player.getVideoTitle().then(function(title) {
+     console.log('title:', title);
+ });
+
+ player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)},1000)); 
+
+ player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
+
+ 
+
+
 
