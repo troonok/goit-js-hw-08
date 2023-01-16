d50adc0d281a8d4653aa8f0cffffd6b5772a5882
Anastasiia Kandalintceva <valeryanaromanova@gmail.com>
2023-01-16 20:06:01 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+    button:document.querySelector('.feedback-form button')
+}
+const STORAGE_KEY ="feedback-form-state";
+const formData =JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+refs.form.addEventListener('input', throttle(saveMessage,500));
+
+function saveMessage(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
+} 
+function submitForm(e){
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)))
+    refs.email.value = null;
+    refs.message.value = null;
+    localStorage.removeItem(STORAGE_KEY);
+}
+refs.form.addEventListener('submit', submitForm);
+
+    if(localStorage.getItem(STORAGE_KEY) !== null)
+    try {
+        const valueForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        refs.email.value = valueForm.email || "";
+        refs.message.value = valueForm.message || "";
+    } catch (error) {
+        console.log(error.name); 
+        console.log(error.message);
+    }
+  
+
 
