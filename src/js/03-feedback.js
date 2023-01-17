a950ec2d0e7167620409afb9e69f3a01e2c85c4e
Anastasiia Kandalintceva <valeryanaromanova@gmail.com>
2023-01-17 20:58:53 +0200

    реализация заполнения формы через цикл forEach

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,30 +6,29 @@ const refs = {
     button:document.querySelector('.feedback-form button')
 }
 const STORAGE_KEY ="feedback-form-state";
-const formData =JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 refs.form.addEventListener('input', throttle(saveMessage,500));
+refs.form.addEventListener('submit', submitForm);
 
 function saveMessage(e) {
     formData[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    
 } 
+
 function submitForm(e){
     e.preventDefault();
     console.log(formData)
-    refs.email.value = null;
-    refs.message.value = null;
+    refs.form.reset();
     localStorage.removeItem(STORAGE_KEY);
-    
 }
-refs.form.addEventListener('submit', submitForm);
 
-    if(localStorage.getItem(STORAGE_KEY) !== null)
+if(localStorage.getItem(STORAGE_KEY) !== null)
     try {
-        
-        refs.email.value = formData.email || "";
-        refs.message.value = formData.message || "";
+        let formValue = Object.entries(formData);
+        formValue.forEach(([name,value]) => {
+            refs.form[name].value = value || ""
+        });
     } catch (error) {
         console.log(error.name); 
         console.log(error.message);
